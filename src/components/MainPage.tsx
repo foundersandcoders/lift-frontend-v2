50e5ee88192c76684453531651ec032baa3e1b5b
Alexander <alex@alexvoiceover.com>
2025-02-17 12:55:47 +0000

    test: reorder main for testing

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -12,17 +12,17 @@ interface MainPageProps {
 const MainPage: React.FC<MainPageProps> = ({ username }) => {
   return (
     <main className='min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-12 '>
+      <h1 className='text-3xl font-bold mb-8 text-center'>
+        Statement Builders for {username}
+      </h1>
       <div className='container mx-auto px-4'>
-        <h1 className='text-3xl font-bold mb-8 text-center'>
-          Statement Builders for {username}
-        </h1>
+        <StatementList username={username} />
+
         <div className='flex flex-col justify-center max-w-3xl mx-auto'>
           <div className='bg-white rounded-xl shadow-lg p-6'>
             <StatementWizard username={username} />
             <StatementBuilder username={username} />
           </div>
-
-          <StatementList username={username} />
         </div>
       </div>
     </main>
